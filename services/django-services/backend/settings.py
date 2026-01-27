d280b396e26b18dddddb0dbeddb82543eb33be5b
Shabad <shabadmk969@outlook.com>
2026-01-27 20:39:33 +0530

    docker setup completed, run pending

--- services/django-services/backend/settings.py
+++ services/django-services/backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-**i+f#t4=n9jwai^(6h#weg5zgbq51%w&k&%e-z1e&t0gd(ax_"
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -78,8 +79,12 @@ WSGI_APPLICATION = "backend.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config("POSTGRES_DB"),
+        "USER": config("POSTGRES_USER"),
+        "PASSWORD": config("POSTGRES_PASSWORD"),
+        "HOST": config("POSTGRES_HOST"),
+        "PORT": config("POSTGRES_PORT"),
     }
 }
 
--- services/node-services/src/server.ts
+++ services/node-services/src/server.ts
@@ -9,12 +9,11 @@ dotenv.config()
 const app = express()
 
 
-const PORT = process.env.PORT as string
+const PORT = Number(process.env.PORT) || 4000;  
 
 const startServer = async () => {
-
-  app.listen(PORT, () => {
-    console.log(`server is running on http://localhost:${PORT}`);
+  app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`);
   });
 };
 
