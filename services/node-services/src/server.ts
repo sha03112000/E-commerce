a71e8480b1b520ba16dcf79a72f8e5d2aa3f669f
Shabad <shabadmk969@outlook.com>
2026-01-28 20:05:27 +0530

    docker setup completed

--- services/node-services/src/server.ts
+++ services/node-services/src/server.ts
@@ -9,6 +9,10 @@ dotenv.config()
 const app = express()
 
 
+app.get("/health", (_, res) => {
+  res.status(200).json({ status: "ok" });
+});
+
 const PORT = Number(process.env.PORT) || 4000;  
 
 const startServer = async () => {
